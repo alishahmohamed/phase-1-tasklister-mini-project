414a54dc1030e3a0e9563b6a633ef7d8e2f61e2f
+ <+>
2022-12-06 15:37:59 +0300

    Completed Assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
+// document.addEventListener("DOMContentLoaded", () => {
+//   // your code here
+//   let form = document.querySelector("form")
+
+//   document.querySelector("form").addEventListener('submit', (e) => {
+//     e.preventDefault();
+//     let inputVal = document.getElementById("new-task-description").value;
+//     const ul = document.getElementById("tasks");
+//     const btn = document.createElement("button");
+//     btn.textContent = "X";
+//     btn.addEventListener("click", handleDelete);
+//     const li = document.createElement("li");
+//     li.textContent = `${inputVal}`;
+//     li.append(btn);
+//     li.append(li);
+//     //form.reset()
+
+//   })
+
+// })
+
+// function handleDelete(e) {
+//   console.log(e.target.parentNode.remove());
+  
+// }
+
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+ // your code here
+  let form = document.querySelector("form")
+  form.addEventListener("submit",function(event){
+    event.preventDefault()
+    let inputHolder = document.createElement("li")
+    let ul = document.querySelector("ul")
+    ul.append(inputHolder)
+    inputHolder.textContent = event.target.newTaskDescription.value})
+  })
